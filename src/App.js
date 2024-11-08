08e14756c71188012fb6f35cfe38d61177a83084
Raveendra Hennyake <raveendralakshan5@gmail.com>
2024-11-08 12:16:00 +0530

    readfunction

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { dirname, join } from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {readFile,writeFile} from 'node:fs';
+import { log } from 'node:console';
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=dirname(__filename);
+readFile(join(__dirname,'new.txt'),'utf-8',(err,data)=>{
+    if(err){
+        console.log(err);
+    }else {
+        console.log(data);
+    }
+})
